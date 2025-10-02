01e7725cef21f51575a5a88949e56a8a5bf0d824
dlph <95047531+DolphOnGitHub@users.noreply.github.com>
2025-10-02 18:00:02 +0800

    Create main8.c

---
+++ EX3/main8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int diameter = 2;
+    double pi = 3.1415926535;
+    double circumference = diameter * pi;
+    
+    printf("%.5f\n", circumference);
+    
+    return 0;
+}
