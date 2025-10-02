8ccf86f190e1d6e72204b1a1b5f7795fedb0881a
dlph <95047531+DolphOnGitHub@users.noreply.github.com>
2025-10-02 17:54:16 +0800

    Create main4.c

---
+++ EX3/main4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i = 10;
+    
+    if (i % 2 == 0) {
+        printf("even number\n");
+    } else {
+        printf("odd number\n");
+    }
+    
+    return 0;
+}
